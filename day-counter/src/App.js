6fb587b3785b9ca867d5ec3f2eb22f2fa732960a
Billy Li <billy1999fans@gmail.com>
2024-05-04 02:46:03 +0200

    Section 6: Challenge 2: Date Counter V2

--- day-counter/src/App.js
+++ day-counter/src/App.js
@@ -31,20 +31,41 @@ function Counter() {
     setCount((count) => count - step);
   }
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (
     <>
       <div className="step">
-        <button onClick={decrementStep}>-</button>
+        {/* <button onClick={decrementStep}>-</button>
         <span>Step: {step}</span>
-        <button onClick={incrementStep}>+</button>
-      </div>
+        <button onClick={incrementStep}>+</button> */}
 
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
+      </div>
       <div className="count">
-        <button onClick={decrementCounter}>-</button>
+        {/* <button onClick={decrementCounter}>-</button>
         <span>Count: {count}</span>
+        <button onClick={incrementCounter}>+</button> */}
+
+        <button onClick={decrementCounter}>-</button>
+        <input
+          type="number"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={incrementCounter}>+</button>
       </div>
-
+      {console.log("count", count)}
       <p>
         <span>
           {count === 0 ? `Today is ${date.toDateString()}` : ""}
@@ -56,6 +77,12 @@ function Counter() {
             : ""}
         </span>
       </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </>
   );
 }
