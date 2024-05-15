3ab84ae9783afcead4606ec4dbdf120ce4dc9792
Billy Li <billy1999fans@gmail.com>
2024-05-15 15:39:05 +0200

    Section 10: usePopCorn

---
+++ usepopcorn/src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { useState } from "react";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
+
+    <Test />
+  </React.StrictMode>
+);
