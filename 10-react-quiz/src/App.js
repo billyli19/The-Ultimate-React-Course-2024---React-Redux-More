2a9223829caf05573e67987b6a893c4fcfcaeb53
Billy Li <billy1999fans@gmail.com>
2024-08-23 20:30:24 +0200

    Add react-quiz

---
+++ 10-react-quiz/src/App.js
@@ -0,0 +1,16 @@
+import { React } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
+    </div>
+  );
+}
